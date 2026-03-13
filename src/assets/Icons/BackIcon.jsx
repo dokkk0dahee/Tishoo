a047b9444bc09340c84c4cb37988d6ae596a22d2
dokkk0dahee <daheebinu622@naver.com>
2026-03-13 15:56:29 +0900

    [STYLE] 뒤로가기 아이콘 생성

---
+++ src/assets/Icons/BackIcon.jsx
@@ -0,0 +1,26 @@
+const BackIcon = ({ className }) => {
+    return (
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="12" 
+        height="20" 
+        viewBox="0 0 12 20" 
+        fill="none"
+        className={className}
+        >
+        <g clipPath="url(#clip0_126_1058)">
+            <path 
+            d="M11.67 1.77L9.9 0L0 9.9L9.9 19.8L11.67 18.03L3.54 9.9L11.67 1.77Z"
+            fill="currentColor" 
+            />
+        </g>
+        <defs>
+            <clipPath id="clip0_126_1058">
+            <rect width="12" height="20" fill="white"/>
+            </clipPath>
+        </defs>
+        </svg>
+    );
+};
+
+export default BackIcon;
