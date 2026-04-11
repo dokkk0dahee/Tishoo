363ed2feef97cc66dc334bcff628e03900771ab4
dokkk0dahee <daheebinu622@naver.com>
2026-04-11 16:45:07 +0900

    [STYLE] 아이콘 적용

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -2,6 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Checklist from "../../components/common/checklist-component";
 import GuideSection from "../../components/common/guidesection-component";
+import GuideIcon from "../../assets/Icons/GuideIcon";
+import NextIcon from "../../assets/Icons/NextIcon";
+import EditIcon from "../../assets/Icons/editIcon";
+
 
 const Home = () => {
     const navigate = useNavigate();
@@ -44,10 +48,7 @@ const Home = () => {
                                 <div><span>발인:</span> 2026년 5월 7일</div>
                                 {/* 우측 하단: 연필 모양 수정 버튼 */}
                                 <button className="w-[32px] h-[32px] bg-[#EDDFCA] rounded-[8px] flex items-center justify-center focus:outline-none transition-colors">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 20H21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                        <path d="M16.5 3.5C16.8978 3.10217 17.4374 2.87868 18 2.87868C18.2786 2.87868 18.5544 2.93355 18.8118 3.04015C19.0692 3.14676 19.3031 3.303 19.5 3.5C19.6969 3.6969 19.8532 3.93081 19.9598 4.1882C20.0664 4.44558 20.1213 4.72143 20.1213 5C20.1213 5.27857 20.0664 5.55442 19.9598 5.8118C19.8532 6.06919 19.6969 6.3031 19.5 6.5L7 19L3 20L4 16L16.5 3.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                    </svg>
+                                    <EditIcon />
                                 </button>
                             </div>
 
@@ -64,9 +65,9 @@ const Home = () => {
                     장례준비, 무엇부터 해야 할까요?
                 </h6>
                 <button 
-                    className="w-full h-[64px] bg-[#EDDFCA] rounded-[8px] border-[1px] border-[#E3E6F0] text-[#0A2472] text-[14px] font-semibold"
+                    className="flex items-center justify-center w-full h-[64px] bg-[#EDDFCA] rounded-[8px] border-[1px] border-[#E3E6F0] text-[#0A2472] text-[14px] font-semibold gap-[8px]"
                     onClick={handleButtonClick}
-                > 단계별 안내 받으러 가기
+                > <GuideIcon /> 단계별 안내 받으러 가기 <NextIcon/>
                 </button>
             </div>
         )}
