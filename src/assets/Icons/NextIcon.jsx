10cd57c7c381ce1f8bd4ead16ee21a9c1824b644
dokkk0dahee <daheebinu622@naver.com>
2026-04-11 16:37:32 +0900

    [STYLE] 아이콘 파일 추가

---
+++ src/assets/Icons/NextIcon.jsx
@@ -0,0 +1,19 @@
+const NextIcon = ({ className }) => {
+    return (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="18" 
+            height="18" 
+            viewBox="0 0 18 18" 
+            fill="none"
+            className={className}
+        >
+            <path 
+                d="M4.41 3.09L10.32 9L4.41 14.91L6 16.5L13.5 9L6 1.5L4.41 3.09Z"
+                fill="currentColor" 
+            />
+        </svg>
+    );
+};
+
+export default NextIcon; 
